82b4c4b64aa005a732fd54b557c844053eddcda2
Saipavankumar Perni <sperni@AffinionDS.Com>
2019-09-24 12:06:10 +0530

    Added purchase and sales entry

--- iia-host/ClientApp/src/app/home/home.component.ts
+++ iia-host/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
---
+++ iia-host/ClientApp/src/app/sales/sales.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales',
+  templateUrl: './sales.component.html',
+  styleUrls: ['./sales.component.css']
+})
+export class SalesComponent {
+}
